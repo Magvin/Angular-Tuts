860c5ffe8244a0ccc9637a48030466dacf8215dd
magvin <zans.litinskis@gmail.com>
2019-02-04 21:21:49 +0000

    ngStyle and if else in angular

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,15 +2,42 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'No Server been created'
+  serverName = '';
+  serverCreated = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true
+    }, 2000)
+  }
 
   ngOnInit() {
   }
 
+
+  onCreateServer = () => {
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName
+
+    setTimeout(() => {
+      this.serverCreated = false;
+    }, 1500)
+
+  }
+
+  onUpdateServerName = (event: Event) => {
+    this.serverName = (<HTMLInputElement>event.target).value;
+
+  }
+  getColor = () => {
+
+  }
 }
