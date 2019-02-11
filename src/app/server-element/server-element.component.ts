b669e9a2a6802ae7278ba56f2e92f10e3f9d5580
magvin <zans.litinskis@gmail.com>
2019-02-11 21:12:42 +0000

    lifecyles hooks

---
+++ src/app/server-element/server-element.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css']
+})
+export class ServerElementComponent implements OnInit, OnChanges {
+  @Input('srvElement') element: { type: string, name: string, content: string };
+  @Input() name: string;
+
+  constructor() {
+    console.log('constructor')
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes)
+  }
+  ngOnInit() {
+    console.log('ngOnInit')
+  }
+  ngDoCheck(): void {
+    //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
+    //Add 'implements DoCheck' to the class.
+    console.log('docheck')
+  }
+  ngAfterContentInit(): void {
+    //Called after ngOnInit when the component's or directive's content has been initialized.
+    //Add 'implements AfterContentInit' to the class.
+    console.log('ngAfterContentInit')
+  }
+
+  ngAfterContentChecked(): void {
+    //Called after every check of the component's or directive's content.
+    //Add 'implements AfterContentChecked' to the class.
+    console.log('ngAfterContentChecked')
+  }
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    console.log('ngAfterViewInit')
+  }
+
+  ngAfterViewChecked(): void {
+    //Called after every check of the component's view. Applies to components only.
+    //Add 'implements AfterViewChecked' to the class.
+    console.log('ngAfterViewChecked')
+  }
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    console.log('ngOnDestroy')
+  }
+}
