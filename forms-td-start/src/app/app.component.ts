2a7356d0b2fb58d218943356bb976a63c3f8cf26
zans.litinskis@gmail.com <zans.litinskis@gmail.com>
2019-03-04 08:21:57 +0000

    radio buttons and suggestName button

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -7,16 +7,32 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    // Another aproach to get values before submitting with @ViewChild
   @ViewChild('f') singupForm: NgForm;
   defaultQuestion = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.singupForm.setValue({
+      userData: {
+          username: suggestedName,
+          email: ''
+      },
+      secret: this.defaultQuestion,
+      questionAnswer: '',
+      gender: 'male'
+
+
+    });
   }
+
+  // Or
   // onSubmit(form: NgForm) {
   //   console.log(form.form.value);
   // }
 
-  // Another aproach to get values before submitting with @ViewChild
+
 
 
 
