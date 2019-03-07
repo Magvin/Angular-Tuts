8fed3b77a892f8f89c95c5f824575c264585afd6
zans.litinskis@gmail.com <zans.litinskis@gmail.com>
2019-03-07 21:12:54 +0000

    custom validator for username

--- forms-reactive-start/forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/forms-reactive-start/src/app/app.component.ts
@@ -8,13 +8,14 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
+  restrictedUsernames =['Chris', 'Ana'];
 
   // Form
   singUpForm: FormGroup;
 
   ngOnInit() {
     this.singUpForm = new FormGroup({
-      'username': new FormControl(null, Validators.required),
+      'username': new FormControl(null, [Validators.required, this.restrictedNames.bind(this)]),
       'email': new FormControl(null, [Validators.email, Validators.required]),
       'gender': new FormControl('male'),
       'hobby': new FormArray([])
@@ -28,5 +29,12 @@ export class AppComponent implements OnInit {
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
    (<FormArray> this.singUpForm.get('hobby')).push(control);
+  }
+
+  restrictedNames(control: FormControl): {[s: string]: boolean} {
+    if(this.restrictedUsernames.indexOf(control.value)) {
+      return {'nameIsRectricted': true};
+    }
+    return null;
   }
 }
